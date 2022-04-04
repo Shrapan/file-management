26a84481f66fbff9e50f5236003f926618132197
Shrapan <samsureshkelkar@gmail.com>
2022-04-04 15:48:47 +0530

    intial setup mapping for function

--- input.js
+++ input.js
@@ -1,2 +1,34 @@
 let inputArr=process.argv.slice(2);
-console.log(inputArr);``
+// console.log(inputArr);
+let command=inputArr[0];
+switch(command){
+    case "tree":
+        treeFn(inputArr[1]);
+        break;
+    case "organise":
+        organiseFn(inputArr[1]);
+        break;
+    case "help":
+        helpFn();
+        break;
+    default:
+        console.log("please 😂 enter valid data");                
+}
+
+function treeFn(dirpath)
+{
+    console.log("you are at tree");
+}
+function organiseFn(dirpath)
+{
+    console.log("you are at organise");
+}
+function helpFn(dirpath)
+{
+    console.log(`
+    List of All commands:
+            1)node input.js tree "dirpath"
+            2)node input.js organise "dirpath"
+            1)node input.js help "dirpath"
+    `)
+}
